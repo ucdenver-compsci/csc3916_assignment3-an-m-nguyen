2d49f726bbf2398cd5f2d7c001487aae09da0f70
An_Nguyen <an.nguyenn2003@gmail.com>
2024-03-10 20:20:52 -0600

    Update

--- server.js
+++ server.js
@@ -86,8 +86,92 @@ router.post('/signin', function (req, res) {
     })
 });
 
+// API route to movies
+router.route('/movies')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        Movie.find({
+            title: { $exists: true, $ne: null },
+            releaseDate: { $exists: true, $ne: null },
+            genre: { $exists: true, $ne: null },
+            actors: { $exists: true, $ne: null }
+        }, (err, movies) => {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.status(200).json(movies);
+            }
+        });
+    })
+    .post(authJwtController.isAuthenticated, function (req, res) {
+        console.log(req.body);
+
+        if (!req.body.title || !req.body.releaseDate || !req.body.genre || !req.body.actors[0] || !req.body.actors[1] || !req.body.actors[2]) {
+            return res.json({ success: false, message: 'Please include all information for title, year released, genre, and 3 actors.'});
+        }
+
+        Movie.findOne({ title: req.body.title }, (err, movie) => {
+            if (movie) {
+                return res.status(400).json({ success: false, message: "That movie already exists." });
+            }
+
+            var movie = new Movie({
+                title : req.body.title,
+                releaseDate : req.body.releaseDate,
+                genre : req.body.genre,
+                actors : req.body.actors
+            });
+
+            movie.save(function (err) {
+                if (err) {
+                    return res.status(403).json({ success: false, message: "Failed to create movie." });
+                } else {
+                    return res.status(200).send({success: true, message: "Successfully created movie."});
+                }
+            });
+        });
+    })
+    .put(authJwtController.isAuthenticated, function (req, res) {
+        console.log(req.body);
+
+        if (!req.body.find_title || !req.body.update_title) {
+            return res.json({ success: false, message: "Please provide a title to be updated as well as the new updated title."});
+        } else {
+            Movie.findOneAndUpdate( req.body.find_title, req.body.update_title, function (err, movie) {
+                if (err) {
+                    return res.status(403).json({success: false, message: "Unable to update title passed in."});
+                } else if (!movie) {
+                    return res.status(400).json({success: false, message: "Unable to find title to update."});
+                } else {
+                    return res.status(200).json({success: true, message: "Successfully updated title."});
+                }
+            });
+        }
+    })
+    .delete(authJwtController.isAuthenticated, function (req, res) {
+        console.log(req.body);
+
+        if (!req.body.find_title) {
+            return res.json({ success: false, message: "Please provide a title to delete." });
+        } else {
+            Movie.findOneAndDelete( req.body.find_title, function (err, movie) {
+                if (err) {
+                    return res.status(403).json({success: false, message: "Unable to delete title passed in."});
+                } else if (!movie) {
+                    return res.status(400).json({success: false, message: "Unable to find title to delete."});
+                } else {
+                    return res.status(200).json({success: true, message: "Successfully deleted title."});
+                }
+            });
+        }
+    })
+    .all(function(req, res) {
+        return res.status(403).json({success: false, message: "This HTTP method is not supported. Only GET, POST, PUT, and DELETE are supported."});
+});
+
+router.all('/', function (req, res) {
+    return res.status(403).json({ success: false, msg: 'This route is not supported.' });
+});
+
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
-
-
